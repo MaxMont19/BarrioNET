4de5e34e77e37e0d4f4d23bdf516a7014dd4f349
MaxMont19 <67833574+MaxMont19@users.noreply.github.com>
2025-02-04 03:25:52 -0500

    Nueva version

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = "Max142521"  # Agrega esta línea
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:TUPASSWORD@basededatoslote.xxxxxx.us-east-2.rds.amazonaws.com:5432/postgres"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:Max142521@basededatoslote.cdeu6e4s2fpd.us-east-2.rds.amazonaws.com:5432/postgres"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
